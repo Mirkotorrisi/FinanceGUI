4edaf02d52e0de85d4dbcce4bdd8595645c894b4
Mirkotorrisi <mirko.torrisi92@gmail.com>
2020-03-27 17:45:37 +0100

    Update Save.java

--- src/financeGUI/Save.java
+++ src/financeGUI/Save.java
@@ -1,54 +1,43 @@
-package financeGUI;
+package finance;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.FileNotFoundException;
-import java.util.LinkedList;
-import java.util.List;
+import java.io.FileOutputStream;
+import java.text.DecimalFormat;
 
-import com.itextpdf.text.DocumentException;
+public interface SavePdF {
+    static void SavePdf(double[][] quotes, FinancialAccounts account) throws FileNotFoundException, DocumentException {
+        //Method to save pdf for each mortgage selected. Uses Pdf Writer
+        DecimalFormat f = new DecimalFormat("##.00");
+        Document document = new Document();
+        PdfWriter.getInstance(document, new FileOutputStream("C:\\Users\\User\\Desktop\\mortgages\\"+account.getName()+" Mortgage.pdf"));
+        document.open();
 
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.VBox;
-import javafx.stage.Modality;
-import javafx.stage.Stage;
+        PdfPTable table = new PdfPTable(4);
+        table.addCell("DATE");
+        table.addCell("REPAYMENT");
+        table.addCell("CAPITAL QUOTE");
+        table.addCell("INTEREST QUOTE");
 
-public class Save {
-	static List<FinancialAccounts> accounts = new LinkedList<FinancialAccounts>();
-	static Stage window = new Stage();
-	static Button button;
-	static Button back;
-	static Scene scene;
-	static String file = "C:\\Users\\LuLele\\Desktop\\MortgagesInfo.csv";
-	
-	public static void display(List<FinancialAccounts> accounts) {
-		//This method let the user choose what mortgage to save as pdf file.
-		Stage window = new Stage();
-		window.initModality(Modality.APPLICATION_MODAL);
-		window.setMinWidth(450);
-		ScrollPane s1 = new ScrollPane();
-		window.setTitle("Save PDF");
-		Label message = new Label("Choose mortgage to Save");
-		VBox box = new VBox();
-		box.getChildren().addAll(message);
-		
-		for(FinancialAccounts account : accounts) {
-			account.adjustRate();
-			account.calcDates();
-			double[][] quotes = account.calculateMortgage();
-			Button button = new Button(account.getName()+ " "+account.getDateStart()+ " "+account.riskProfile);
-			button.setOnAction(e->{ 
-				try {savePdf.savePdf(quotes,account);} 
-				catch (FileNotFoundException | DocumentException e1) {AlertBox.display("Error", e1.getMessage());}
-				AlertBox.display("", "Pdf saved for "+account.getName());});
-				
-			box.getChildren().addAll(button);
-			}
-		
-		s1.setContent(box);
-		scene = new Scene (s1, 190, 400);
-		window.setScene(scene);
-		window.showAndWait();
-	}
+        for(int i = 0; i < quotes[0].length; i++) {
+            account.adjustRate();
+            table.addCell(account.calcDates()[i]);
+            table.addCell(f.format(quotes[0][i]));
+            table.addCell(f.format(quotes[1][i]));
+            table.addCell(f.format(quotes[2][i]));
+        }
+        document.add(table);
+        Phrase phrase = new Phrase();
+        phrase.add("Name: "+ account.getName()+"\nMortgage: "+ account.mortgage+"$");
+        phrase.add("\nAmmortization: "+ account.mortType);
+        phrase.add("\n"+account.riskProfile + " profile");
+        document.add(phrase);
+        document.close();
+
+    }
 }
